abde3c083dd911470a82a1b8b17a21a6adfc227e
Preston O'Neal <prestononeal@gmail.com>
2016-06-19 00:35:41 -0700

    Add standard queries for Place model

--- app/models/place.rb
+++ app/models/place.rb
@@ -28,6 +28,35 @@ class Place
     collection.insert_many data_hash
   end
 
+  # Returns a Mongo view with a query to match documents with a short_name
+  def self.find_by_short_name(name)
+    collection.find({"address_components.short_name": name})
+  end
+
+  # Converts a Mongo view to an array of Place objects
+  def self.to_places(view)
+    view.map { |p| Place.new(p.to_hash) }
+  end
+
+  # Finds a place in the DB by ID and returns an initialized Place object for it
+  def self.find(id)
+    pl = collection.find({:_id=>BSON::ObjectId.from_string(id)})
+    return nil if pl.count == 0 else Place.new(pl.first)
+  end
+
+  # Return an instance of all documents as Place instances
+  def self.all(offset=nil, limit=nil)
+    pls = collection.find()
+    pls = pls.skip(offset) if !offset.nil?
+    pls = pls.limit(limit) if !limit.nil?
+    self.to_places(pls)
+  end
+
+  # Destroys the document associated with this Place's id
+  def destroy
+    self.class.collection.find(:_id=>BSON::ObjectId.from_string(@id)).delete_one
+  end
+
   # Convenience method for retrieving address_components from the collection
   #  * sort (optional):
   #  * offset (optional): document number to start results
@@ -63,5 +92,30 @@ class Place
     aggs << {:$project=>{:_id=>1}}
     ids = collection.find().aggregate(aggs)
     ids.to_a.map {|i| i[:_id].to_s}
+  end
+
+  # Create a 2dsphere index to the collection for the geometry.geolocation property
+  def self.create_indexes
+    collection.indexes.create_one({"geometry.geolocation"=>Mongo::Index::GEO2DSPHERE})
+  end
+
+  # Remove the 2dsphere index from the collection
+  def self.remove_indexes
+    collection.indexes.drop_one("geometry.geolocation_2dsphere")
+  end
+
+  # Finds places near a given Point
+  def self.near(pt, max_meters=nil)
+    collection.find("geometry.geolocation": {
+      :$near=>{
+        :$geometry=>pt.to_hash,
+        :$maxDistance=>max_meters
+      }
+    })
+  end
+
+  # Finds places near this Point
+  def near(max_meters=nil)
+    self.class.near(self, max_meters)
   end
 end
